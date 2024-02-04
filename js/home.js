f8897a5136a554b5cb6bc7780b748e5f72db83e4
snhrayk <ayaka21sunahara@gmail.com>
2024-02-04 12:35:22 +0900

    ホームとログイン画面

--- js/home.js
+++ js/home.js
@@ -1,1 +1,22 @@
-// const noticeMenu = document.querySelector(".notice");
+// モーダルとボタンとクローズボタンの要素を取得
+var modal = document.querySelector("#modalMW");
+var btn = document.querySelector("#postBtn");
+var span = document.querySelector(".close")[0];
+
+// ボタンをクリックしたときにモーダルを表示
+
+// btn.onclick = function () {
+//   modal.style.display = "block";
+// };
+
+// クローズボタンをクリックしたときにモーダルを非表示
+// span.onclick = function () {
+//   modal.style.display = "none";
+// };
+
+// ユーザーがモーダルの外側をクリックしたときにモーダルを非表示
+// window.onclick = function (event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// };
